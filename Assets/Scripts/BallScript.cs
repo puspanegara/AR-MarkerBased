69b379b98c093464bc631530dc3a4d1801894447
puspanegara <54850975+puspanegara@users.noreply.github.com>
2020-05-03 19:47:13 +0700

    Mekanin dan Gameplay dasar
    Game roll the ball dengan menggukan AR marker based sudah bisa dimainkan untuk melihat gameplay dan mekanik dasarnya.
    
    Saat bola berhasil sampai ke kotak merah, maka akan lanjut ke level berikutnya.

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallScript : MonoBehaviour
+{
+    public GameObject plane;
+    public GameObject spawnPoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < plane.transform.position.y - 1)
+        {
+            transform.position = spawnPoint.transform.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AR MarkerBased
  Game with AR Marker Based using Vuforia
+
+Game RolltheBall merupakan game yang dirancang untuk dimainkan oleh dua orang pemain.
+Dimana pemain yang memegang hp akan memberi arahan kepada pemain yang memegan kertas atau papan yang terdapat gambar.
+Sehingga dibutuhkan kerja sama tim yang baik untuk bisa menyekesaikan game yang satu ini.
+
+Game ini menggunakan Game Enginee Unity 2019.1.2 
