2a93a7e9ab98c188fd96195cf3d1bef7c638f8c3
puspanegara <54850975+puspanegara@users.noreply.github.com>
2020-05-03 20:18:35 +0700

    tes

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    //Inisialisasi GameObject plane
+    public GameObject plane;
+    //Inisialisasi GameObject spawn 
+    public GameObject spawnPoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Menentukan posisi bola saat akan spawn
+        if(transform.position.y < plane.transform.position.y - 1)
+        {
+            transform.position = spawnPoint.transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/Cek.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cek : MonoBehaviour
+{
+    
+    //Menggunakan int untuk memamnggil scene yang akan digunakan
+    public int numberScene;
+
+    /*Membuat methode saat bola mengenai kotak merah akan berganti
+    ke Level selanjutnya*/
+    void OnTriggerEnter(Collider other) 
+    {
+        //Saat mendeteksi Tag Ball
+        if(other.CompareTag("Ball"))
+        {
+            //Scene yang di tuju akan di ganti
+            SceneManager.LoadScene(numberScene);
+        }    
+    }
+}
