8172977534696c28713f281f70a6fa5606fc73e4
puspanegara <54850975+puspanegara@users.noreply.github.com>
2020-06-21 14:46:30 +0700

    Level 3
    - Terdapat coll yang berwarna lain
    - Jika tersentuh akan kalan
    - silahkan di coba

---
+++ Assets/Scripts/CollEnemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollEnemy : MonoBehaviour
+{
+    public GameObject losePanel;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Ball")
+        {
+            losePanel.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/SlashtoMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SlashtoMenu : MonoBehaviour
+{
+    public static int SceneNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(SceneNumber == 0)
+        {
+            StartCoroutine(ToSplashTwo());
+        }
+        if (SceneNumber == 1)
+        {
+            StartCoroutine(ToMenu());
+        }
+    }
+
+    IEnumerator ToSplashTwo()
+    {
+        yield return new WaitForSeconds(15);
+        SceneNumber = 1;
+        SceneManager.LoadScene(1);
+    }
+
+    IEnumerator ToMenu()
+    {
+        yield return new WaitForSeconds(8);
+        SceneNumber = 2;
+        SceneManager.LoadScene(2);
+    }
+}
